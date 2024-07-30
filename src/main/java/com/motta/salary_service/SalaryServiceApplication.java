6143dade6151010d862bb3d4fc51f265b14b1095
arunpt7777@gmail.com <arunpt7777@gmail.com>
2024-07-30 21:02:56 +0530

    US30- Add Swagger to all projects

--- src/main/java/com/motta/salary_service/SalaryServiceApplication.java
+++ src/main/java/com/motta/salary_service/SalaryServiceApplication.java
@@ -2,12 +2,24 @@ package com.motta.salary_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SalaryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalaryServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("http://localhost:3000");
+			}
+		};
 	}
 
 }
